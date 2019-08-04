24f01304cb632da457e6a5a54b2980da966cc911
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-08-04 16:15:51 -0400

    Update rsync.sh
    source: https://www.cyberciti.biz/faq/bash-for-loop/

--- rsync.sh
+++ rsync.sh
@@ -1,1 +1,5 @@
+for (( ; ; )) #comment to make it a run-once thing
+do #comment to make it a run-once thing
+   echo "infinite loops [ hit CTRL+C to stop]" #comment to make it a run-once thing
+done #comment to make it a run-once thing
 rsync -aE SourcePath DestinationPath
