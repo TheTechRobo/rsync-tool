60c8586d0ac84f25839785264a7baa15887f8326
TtR <52163910+TheTechRobo@users.noreply.github.com>
2019-08-08 16:08:06 -0400

    Update rsync.sh
    • now you know it's doing **something**
    • tells you how to stop

--- rsync.sh
+++ rsync.sh
@@ -1,4 +1,5 @@
 for (( ; ; )) #comment to make it a run-once thing
 do #comment to make it a run-once thing
    rsync -aE --delete SourcePath DestinationPath #comment to make it a run-once thing
+   echo Quit the terminal and press return to stop
 done #comment to make it a run-once thing
